df569d8fe835eaf644353e5c057b83fda14c2574
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2016-11-11 04:20:16 +0100

    bump patch version number

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-export { Driver } from './driver';
-export { Session } from './session';
-export { Node, Relationship, UnboundRelationship, Path, PathSegment } from './graph-types';
-export { Record } from './record';
-export { Neo4jError } from './error';
-export { Result } from './result';
-export { ResultSummary } from './result-summary';
-export { Transaction } from './transaction';
+declare module Neo4J {
+	export { Driver } from './driver';
+	export { Session } from './session';
+	export { Node, Relationship, UnboundRelationship, Path, PathSegment } from './graph-types';
+	export { Record } from './record';
+	export { Neo4jError } from './error';
+	export { Result } from './result';
+	export { ResultSummary } from './result-summary';
+	export { Transaction } from './transaction';
+}
